e97e04ee86fbe8061b3f4a1f8f8c0866b3bcc3e6
davidseong8914 <davidseong8914@gmail.com>
2025-03-22 01:50:51 -0400

    Add imu and lidar to robot

--- src/setup_path/sensors/launch/sensors-service.launch.py
+++ src/setup_path/sensors/launch/sensors-service.launch.py
@@ -8,6 +8,23 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
+    # Include Packages
+
+    # Declare launch files
+    launch_file_lidar3d_0 = '/home/dseong/vegmap/src/setup_path/sensors/launch/lidar3d_0.launch.py'
+    launch_file_imu_0 = '/home/dseong/vegmap/src/setup_path/sensors/launch/imu_0.launch.py'
+
+    # Include launch files
+    launch_lidar3d_0 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([launch_file_lidar3d_0]),
+    )
+
+    launch_imu_0 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([launch_file_imu_0]),
+    )
+
     # Create LaunchDescription
     ld = LaunchDescription()
+    ld.add_action(launch_lidar3d_0)
+    ld.add_action(launch_imu_0)
     return ld
