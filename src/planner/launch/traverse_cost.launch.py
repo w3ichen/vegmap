7fb8ee60775d32bac0fd026646f0af281ea139cd
patrick_laptop <email@email.com>
2025-04-20 12:32:51 -0400

    dVID'S CODE

--- src/planner/launch/traverse_cost.launch.py
+++ src/planner/launch/traverse_cost.launch.py
@@ -68,24 +68,12 @@ def generate_launch_description():
     )
     ld.add_action(costmap_visualizer)
 
-
-    # # robot_pose_tf_publisher.py
-    # robot_pose_tf_node = Node(
-    #     package='planner',
-    #     executable='robot_pose_tf_publisher',
-    #     name='robot_pose_tf_publisher',
-    #     output='screen',
-    #     parameters=[{'use_sim_time': True}],
-    # )
-    # ld.add_action(robot_pose_tf_node)
-
     # transform resistance_zones to map frame
     tf_static_publisher = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='static_transform_publisher',
         arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom'],
-        # arguments=['0', '0', '0', '0', '0', '0', 'map', 'resistance_zones'],
         output='screen'
     )
     ld.add_action(tf_static_publisher)
